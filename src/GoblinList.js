3f3b10098822bc059e379dfacaec1fa860d9d570
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-04 14:56:42 -0700

    GoblinForm accepts all the props that App passes it. Input and select values are equal to goblinForm state. On change, input and select fields update goblinForm state. On form submit, submitGoblin is called

--- src/GoblinList.js
+++ src/GoblinList.js
@@ -5,6 +5,7 @@ export default function GoblinList({ goblins, handleDeleteGoblin }) {
   return (
     <div className="goblin-list quarter">
       {/* map over your goblins and render out a Goblin component for each goblin. You've seen this before. The only difference here is that you need to pass handleDeleteGoblin (a prop that is a function), as well */}
+      {console.log(goblins)}
       {goblins.map((goblin, i) => (
         <Goblin key={goblin.name + i} goblin={goblin} handleDeleteGoblin={handleDeleteGoblin} />
       ))}
