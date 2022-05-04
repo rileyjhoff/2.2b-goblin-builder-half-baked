ad5eef6736c1dfeea0c71d40c6c2d3c5bd5fe8e7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-04 12:27:43 -0700

    initialized state in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import GoblinForm from './GoblinForm';
 import GoblinList from './GoblinList';
 import Goblin from './Goblin';
+import { useState } from 'react';
 
 function App() {
   /* 
@@ -12,6 +13,12 @@ function App() {
       goblinFormHP, which is how we track the user input for the current HP of the goblin in the form
       goblinFormColor, which is how we track the user input for the current color of the goblin in the form
 */
+
+  const [allGoblins, setAllGoblins] = useState([]);
+  const [filteredGoblins, setFilteredGoblins] = useState(allGoblins);
+  const [goblinFormName, setGoblinFormName] = useState('');
+  const [goblinFormHP, setGoblinFormHP] = useState(1);
+  const [goblinFormColor, setGoblinFormColor] = useState('');
 
   function submitGoblin(e) {
     e.preventDefault();
