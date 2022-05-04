7d7ea1eea972e3bcdae2c5130b16ea0cf9ae8b2a
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-04 15:30:53 -0700

    added a state for search. App conditionally passes GoblinList filteredGoblins or allGoblins as the goblins prop depending on whether or not there is a search value

--- src/App.js
+++ src/App.js
@@ -18,13 +18,16 @@ function App() {
   const [goblinFormName, setGoblinFormName] = useState('');
   const [goblinFormHP, setGoblinFormHP] = useState('');
   const [goblinFormColor, setGoblinFormColor] = useState('lightgreen');
+  const [search, setSearch] = useState('');
 
   function submitGoblin(e) {
     e.preventDefault();
     // on submit, make a new goblin object with a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
     const newGoblin = { name: goblinFormName, hp: goblinFormHP, color: goblinFormColor };
     // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
-    setAllGoblins([...allGoblins, newGoblin]);
+    allGoblins.push(newGoblin);
+    setAllGoblins([...allGoblins]);
+    setFilteredGoblins([...allGoblins]);
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
     setGoblinFormName('');
     setGoblinFormHP('');
@@ -68,7 +71,13 @@ function App() {
       <div className="goblin-filter quarter">
         Filter Goblins
         {/* note that handleFilterGoblins is defined upstairs. This is where the allGoblins array gets filtered */}
-        <input onChange={(e) => handleFilterGoblins(e.target.value)} />
+        <input
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            handleFilterGoblins(e.target.value);
+          }}
+        />
       </div>
       <GoblinForm
         /*
@@ -91,7 +100,7 @@ function App() {
         setGoblinFormColor={setGoblinFormColor}
       />
       <GoblinList
-        goblins={filteredGoblins.length > 0 ? filteredGoblins : allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
+        goblins={search ? filteredGoblins : allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
